0b5bd6c1360a01b48347a3be45c640d2f88f4ec1
Programming Hero <programming.hero1@gmail.com>
2023-10-01 22:03:25 -0500

    added login route

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/career">Career</NavLink></li>
-
+        <li><NavLink to="/login">Login</NavLink></li>
     </>
 
     return (
@@ -34,7 +34,7 @@ const Navbar = () => {
                         <img src={userDefaultPic} />
                     </div>
                 </label>
-                <Link>
+                <Link to="/login">
                     <button className="btn">Login</button>
                 </Link>
             </div>
