208d58f225b90184861dea584ad5358d277b11cc
Programming Hero <programming.hero1@gmail.com>
2023-10-02 20:42:04 -0500

    firebase deploy

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/career">Career</NavLink></li>
+        <li><NavLink to="/addict">Addict</NavLink></li>
         <li><NavLink to="/login">Login</NavLink></li>
     </>
 
