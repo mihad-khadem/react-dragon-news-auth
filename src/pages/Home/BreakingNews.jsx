9789f17b0e5e3590f4260581f1b5e51ec5fd88ec
mihad-khadem <mihadkhadem@gmail.com>
2024-01-02 13:54:44 +0600

    Deployed in firebase

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -6,9 +6,9 @@ const BreakingNews = () => {
         <div className="flex">
             <button className="btn btn-secondary">Breaking News</button>
             <Marquee pauseOnHover={true} speed={100}>
-                <Link className="mr-12" to="/">I can be a React component, multiple React components.....</Link>
-                <Link className="mr-12" to="/">I can be a React component, multiple React components.....</Link>
-                <Link className="mr-12" to="/">I can be a React component, multiple React components.....</Link>
+                <Link className="mr-12" to="/">Biden Pledges Nearly $3 Billion To Ukraine In Largest U.S. Military Aid Package Yet.....</Link>
+                <Link className="mr-12" to="/">Tucker Carlson Swears Vladimir Putin Is Winning The War In Ukraine.....</Link>
+                <Link className="mr-12" to="/">U.S. concerned about India's participation in Russia military exercises.....</Link>
             </Marquee>
         </div>
     );
